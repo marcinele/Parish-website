e5b721462372d32adfe9da42ba8f12b4140962aa
AdrianC2000 <adrianciesielczyk2000@gmail.com>
2021-06-03 22:53:47 +0200

    Added cool feature

--- js/home.js
+++ js/home.js
@@ -7,3 +7,13 @@ $(function () {
         }
     });
 });
+$(function() {
+    $("ul li a").mouseover(function(){
+        $("ul li a").css("opacity", "0.3");
+        $(this).css("opacity", "1");
+    });
+    $("ul li a").mouseout(function(){
+        $("ul li a").css("opacity", "1");
+    });
+
+});
