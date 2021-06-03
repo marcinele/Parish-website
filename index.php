86a3b89b883c9f3f81ff76cb0334353d3a94068f
marcinele <marcinten@gmail.com>
2021-06-03 17:15:51 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+ini_set('display_errors', 1); // Przenieść errory do jakiegoś loggera na końcu roboty
+error_reporting(E_ALL);
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('html');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('index.html', array(
+        'variable' => 'Twig',
+));
+
