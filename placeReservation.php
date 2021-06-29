e7be5f2bfdabad20da3335d95993ab9e6f4b8a54
AdrianC2000 <adrianciesielczyk2000@gmail.com>
2021-06-29 10:30:06 +0200

    placeReservation developing

--- placeReservation.php
+++ placeReservation.php
@@ -1,6 +1,19 @@
 <?php
 
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $stmt = $dbh->prepare("SELECT * FROM masses WHERE id = :id");
+    $stmt->execute([':id' => $id]);
+    $picked_mass = $stmt->fetch(PDO::FETCH_ASSOC);
+    $date = $picked_mass['date'];
+    $hour = $picked_mass['hour'];
+    $author = $picked_mass['author'];
+}
+
 echo $twig->render('placeReservation.html', [
     'post' => $_POST,
     'session' => $_SESSION,
-    'get' => $_GET]);
+    'get' => $_GET,
+    'date' => $date,
+    'hour' => $hour,
+    'author' => $author,]);
--- reservations.php
+++ reservations.php
@@ -1,6 +1,51 @@
 <?php
 
+if (isset($_POST['chosenDate'])) {
+    $date = $_POST['chosenDate'];
+    $stmt = $dbh->prepare("SELECT * FROM masses WHERE date = :date");
+    $stmt->execute([':date' => $date]);
+    $all_hours = "";
+    $all_dates = array();
+    while ($masses = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $actual_data = array();
+        array_push($actual_data, $masses['id'], $masses['date'], $masses['hour'], $masses['author']);
+        $all_hours = $all_hours . " " . substr($masses['hour'], 0, 5);
+        array_push($all_dates, $masses['date']);
+    }
+    echo print_r($all_hours);
+    exit;
+}
+
+/*if (isset($_POST['dateAndTimeSubmit'])) {
+    $date = $_POST['date'];
+    $hour = $_POST['time'];
+    $stmt = $dbh->prepare("SELECT * FROM masses WHERE date = :date AND hour = :hour");
+    $stmt->execute([':date' => $date, ':hour' => $hour]);
+    $picked_mass = $stmt->fetch(PDO::FETCH_ASSOC);
+    $id = $picked_mass['id'];
+    echo '<script>';
+    echo 'console.log(' . json_encode($id) . ')';
+    echo '</script>';
+}*/
+
+if (isset($_POST['actualDate']) && isset($_POST['actualTime'])) {
+    $date = $_POST['actualDate'];
+    $hour = $_POST['actualTime'];
+    $stmt = $dbh->prepare("SELECT * FROM masses WHERE date = :date AND hour = :hour");
+    $stmt->execute([':date' => $date, ':hour' => $hour]);
+    $picked_mass = $stmt->fetch(PDO::FETCH_ASSOC);
+    $id = $picked_mass['id'];
+    echo print_r($id);
+    exit;
+}
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    echo"<script> console.log($id) </script>";
+}
+
 echo $twig->render('reservations.html', [
     'post' => $_POST,
     'session' => $_SESSION,
     'get' => $_GET]);
+
